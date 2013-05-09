a4b43a20e8c80a8a64ada564862a78c8114e2f0f
ytzong <ytzong7@gmail.com>
2013-05-09 10:18:46 +0800

    Initial Commit

---
+++ FXLabelCrash/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FXLabelCrash
+//
+//  Created by ytzong on 13-5-9.
+//  Copyright (c) 2013年 ytzong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FXLabelCrash/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FXLabelCrash
+//
+//  Created by ytzong on 13-5-9.
+//  Copyright (c) 2013年 ytzong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
